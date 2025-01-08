183b87494ec597d4611b02abb3851539c6e60d0f
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2025-01-08 11:16:15 +0530

    Added update view and action

--- Controllers/FriendController.cs
+++ Controllers/FriendController.cs
@@ -33,6 +33,26 @@ public class FriendController : Controller
         return View();
     }
 
+    public IActionResult Update(Guid id){
+        ViewBag.friend = FriendService.FindFriend(id);
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Update(Guid id, Friend friend){
+        friend.FriendId = id;
+        FriendService.UpdateFriend(friend);
+        ViewBag.friend = FriendService.FindFriend(id);
+        return View();
+    }
+
+
+    public IActionResult Delete(Guid id){
+        FriendService.RemoveFriend(id);
+        ViewBag.friends = FriendService.GetFriendList();
+        return View("Index");
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
