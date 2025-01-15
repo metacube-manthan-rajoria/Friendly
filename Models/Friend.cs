dc55aa85f88f67829eb8a117016cb5498d3f8d5f
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2025-01-15 14:28:32 +0530

    Client side errors now work

--- Models/Friend.cs
+++ Models/Friend.cs
@@ -4,7 +4,7 @@ public class Friend{
     [Key]
     [Required]
     public Guid FriendId {get; set;}
-    [Required]
+    [Required(ErrorMessage ="Friend name empty not allowed.")]
     [StringLength(25, ErrorMessage = "Friend name empty not allowed.")]
     public string? FriendName {get; set;}
     [StringLength(25, ErrorMessage = "Max length is 25 Characters")]
